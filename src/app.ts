38917a1e2197cd2fa904c89965b2536e6a1da0d1
3-24 <dev.youngseok@gmail.com>
2021-06-26 21:23:21 +0900

    Create shutdown command

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
-console.log("Hello World");
+import { Client, Message } from "discord.js";
+import {bot_token, owner_id} from "../config.json";
+let client = new Client();
+client.on('message', (message: Message) => {
+    const prefix = 'bet.'
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    const args = message.content.slice(prefix.length).trim().split(' ');
+    const command = args.shift();
+    if (command == "shutdown"){
+        if (message.author.id == owner_id){
+            message.channel.send("Shutting down...").then(m=>{
+                client.destroy();
+            });
+        }
+    }
+});
+client.login(bot_token);
