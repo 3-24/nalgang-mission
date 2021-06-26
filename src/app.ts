68629b2607295c7f0e99ec0ce3333fe1fe98559a
3-24 <dev.youngseok@gmail.com>
2021-06-26 21:28:25 +0900

    Create help command template

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,25 @@
 import { Client, Message } from "discord.js";
 import {bot_token, owner_id} from "../config.json";
 let client = new Client();
+
+client.once('ready', () => {
+    if (client.user == null){
+        console.log("FATAL");
+        return;
+    }
+    client.user.setActivity("bet.help", {type: "LISTENING"})
+});
+
 client.on('message', (message: Message) => {
     const prefix = 'bet.'
     if (!message.content.startsWith(prefix) || message.author.bot) return;
     const args = message.content.slice(prefix.length).trim().split(' ');
     const command = args.shift();
-    if (command == "shutdown"){
+    if (command == "help"){
+        message.channel.send("On construction")
+        // TODO
+    }
+    else if (command == "shutdown"){
         if (message.author.id == owner_id){
             message.channel.send("Shutting down...").then(m=>{
                 client.destroy();
