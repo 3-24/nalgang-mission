1b69f6d28e55847c972a72ac72dda0ffe7b5f484
jwpee <jwpee0228@gmail.com>
2021-07-04 22:47:48 +0900

    Add case for first bet in isFenceSitter

--- src/database.ts
+++ src/database.ts
@@ -25,24 +25,24 @@ async function addGame(title:string, desc:string, user_id:string, guild_id:strin
         [title, desc, user_id, guild_id, status]);
 }
 
-async function db_get(query:string){
+async function getSuccess(query:string){
     return new Promise(function(resolve,reject){
-        db.get(query, function(err:any,rows:any){
+        db.get(query, function(err:any,res:any){
            if(err){return reject(err);}
-           resolve(rows["success"]);
+           resolve(res? res["success"]: 2);
          });
     });
 }
 
-
 async function isFenceSitter(user_id:string, game_id:number, success:boolean){
     const int_success = success ? 1 : 0;
     const query = 'SELECT success FROM Bet WHERE user_id = '+user_id+' AND game_id ='+game_id;
-    const get_db = await db_get(query);
+    const get_db = await getSuccess(query);
     console.log(get_db);
-    if(get_db === int_success){console.log("!");return true;}
+    if(get_db == int_success || get_db == 2) return true;
     else return false
 }
+
 async function addBet(title:string, user_id:string, success:boolean, point:number){
     const int_success = success ? 1 : 0;
 }
@@ -52,7 +52,8 @@ async function main(){
     //db.exec(`INSERT INTO Bet(game_id, user_id, success,bet_point) VALUES (1,'1',1,1),(1,'1',1,2),(1,'2',0,1),(1,'2',0,2);`);
     const test = await isFenceSitter('1',1,true);
     const test2 = await isFenceSitter('1',1,false);
-    console.log(test,test2);
+    const test3 = await isFenceSitter('3',1,true);
+    console.log(test,test2, test3);
 }
 
 main();
